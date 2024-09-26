7d7e75997c4df6a3e5fb7db3f7b238fec08226ad
Evann Jacquot <evann.jacquot@artfx.fr>
2024-09-26 14:46:10 +0200

    [add] item lists

--- GameManager.cpp
+++ GameManager.cpp
@@ -4,12 +4,38 @@
 
 GameManager::GameManager()
 {
-	mShowInventory = true;
-	inventoryTab = ItemTag::armor;
-	allInventories[0] = Inventory(50, 100);
-	allInventories[1] = Inventory(20, 100);
-	allInventories[2] = Inventory(20, 100);
-	allInventories[3] = Inventory(20, 100);
+	mButtonIndex = 0;
+	mShowInventory = false;
+	mInventoryTab = ItemTag::armor;
+	mAllInventories[0] = Inventory(50, 100, ItemTag::armor);
+	mAllInventories[1] = Inventory(20, 100, ItemTag::weapon);
+	mAllInventories[2] = Inventory(30, 100, ItemTag::consumable);
+	mAllInventories[3] = Inventory(25, 100, ItemTag::craftingItem);
+	mInventoryButtons[0] = Buttons({ 95, 124, 215, 50}, BLUE, "Armor", BLACK, 25);
+	mInventoryButtons[1] = Buttons({ 320, 124, 215, 50 }, BLUE, "Weapons", BLACK, 25);
+	mInventoryButtons[2] = Buttons({ 545, 124, 215, 50 }, BLUE, "Consumable", BLACK, 25);
+	mInventoryButtons[3] = Buttons({ 770, 124, 215, 50 }, BLUE, "Craft Objects", BLACK, 25);
+	mInventoryButtons[mButtonIndex].SetButtonColor(DARKBLUE);
+
+	mArmorItems[0] = ;
+	mArmorItems[1] = ;
+	mArmorItems[2] = ;
+	mSword = ;
+	mBow = ;
+	mConsumableItems[0] = ;
+	mConsumableItems[1] = ;
+	mCraftingItems[0] = ;
+	mCraftingItems[1] = ;
+
+	mAllItems[0] = mArmorItems[0];
+	mAllItems[1] = mArmorItems[1];
+	mAllItems[2] = mArmorItems[2];
+	mAllItems[3] = mSword;
+	mAllItems[4] = mBow;
+	mAllItems[5] = mConsumableItems[0];
+	mAllItems[6] = mConsumableItems[1];
+	mAllItems[7] = mCraftingItems[0];
+	mAllItems[8] = mCraftingItems[1];
 }
 
 GameManager::~GameManager()
@@ -19,7 +45,7 @@ GameManager::~GameManager()
 void GameManager::Load()
 {
 	for (int i = 0; i < 4; i++) {
-		allInventories[i].RefreshInventory();
+		mAllInventories[i].RefreshInventory();
 	}
 }
 
@@ -27,22 +53,56 @@ void GameManager::Update()
 {
 	if (IsKeyPressed(KEY_TAB)) {
 		(mShowInventory == false) ? mShowInventory=true : mShowInventory=false;
+		for (int i = 0; i < 4; i++) {
+			mInventoryButtons[i].ResetTimer();
+		}
 	}
 	if (mShowInventory) {
-		allInventories[static_cast<int>(inventoryTab)].Update();
+		mAllInventories[static_cast<int>(mInventoryTab)].Update();
+		for (int i = 0; i < 4; i++) {
+			mInventoryButtons[i].Update();
+			if (mInventoryButtons[i].GetClickedBool()) {
+				mInventoryButtons[i].SetButtonColor(DARKBLUE);
+				mInventoryButtons[mButtonIndex].SetButtonColor(BLUE);
+				mButtonIndex = i;
+				mInventoryButtons[i].SetClickedBool(false);
+				mInventoryTab = static_cast<ItemTag>(i);
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < 9; i++) {
+			mAllItems[i]->Update();
+			if (mAllItems[i]->GetIsClicked()) {
+				mAllItems[i]->SetIsClicked(false);
+				for (int j = 0; j < 4; j++) {
+					if (mAllInventories[j].GetInventoryTag() == mAllItems[i]->GetTag()) {
+						mAllInventories[j].AddItemToInventory(mAllItems[i]);
+					}
+				}
+			}
+		}
 	}
 }
 
 void GameManager::Draw()
 {
 	if (mShowInventory) {
-		allInventories[static_cast<int>(inventoryTab)].Draw();
-	}	
+		mAllInventories[static_cast<int>(mInventoryTab)].Draw();
+		for (int i = 0; i < 4; i++) {
+			mInventoryButtons[i].Draw();
+		}
+	}
+	else {
+		for (int i = 0; i < 9; i++) {
+			mAllItems[i]->Draw();
+		}
+	}
 }
 
 void GameManager::Unload()
 {
 	for (int i = 0; i < 4; i++) {
-		allInventories[i].Unload();
+		mAllInventories[i].Unload();
 	}
 }
--- GameManager.h
+++ GameManager.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "raylib.h"
 #include "Inventory.h"
+#include "Armor.h"
+#include "Melee.h"
+#include "Ranged.h"
+#include "Consumable.h"
+#include "CraftingMaterial.h"
+#include "Buttons.h"
 class GameManager
 {
 public:
@@ -12,7 +18,18 @@ public:
 	void Unload();
 private:
 	bool mShowInventory;
-	ItemTag inventoryTab;
-	Inventory allInventories[4];
+	ItemTag mInventoryTab;
+	Inventory mAllInventories[4];
+	Buttons mInventoryButtons[4];
+
+	Item* mAllItems[9];
+
+	Armor* mArmorItems[3];
+	Melee* mSword;
+	Ranged* mBow;
+	Consumable* mConsumableItems[2];
+	CraftingMaterial* mCraftingItems[2];
+
+	int mButtonIndex;
 };
 
